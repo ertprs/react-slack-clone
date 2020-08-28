d9b0c88022519d3962efcc9de20c8de2e5455fa3
venaz-dev <chibuzo.madu90@gmail.com>
2020-08-28 10:13:53 +0100

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+import firebase from 'firebase/app'
+import "firebase/auth"
+import "firebase/database"
+import "firebase/storage"
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyC1282dob5bL8dp-1PnPj35x1Hk2iLSj90",
+    authDomain: "dev-chat-9be95.firebaseapp.com",
+    databaseURL: "https://dev-chat-9be95.firebaseio.com",
+    projectId: "dev-chat-9be95",
+    storageBucket: "dev-chat-9be95.appspot.com",
+    messagingSenderId: "170517851966",
+    appId: "1:170517851966:web:04ea5362dba8c3a033066f"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+
+export default firebase
